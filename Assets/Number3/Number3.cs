0fd183757908dd05ca45e4132529a2f53b78c943
ohishi_MSI2023 <gpu.kenji.ohishi@gmail.com>
2024-05-10 18:05:55 +0900

    ３の倍数なら勝ちシステム

--- Assets/Number3/Number3.cs
+++ Assets/Number3/Number3.cs
@@ -1,26 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Number3 : MonoBehaviour
 {
-    Text numLabel;
-    Text resultLabel;
+    public Text numLabel;
+    public Text resultLabel;
 
     int num;
     bool sw;
 
-    // Start is called before the first frame update
     void Start()
     {
         sw = false;
+        num = -999;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Return))
+        if(Input.GetMouseButtonDown(0))
         {
             sw = (sw) ? false : true;
         }
@@ -28,11 +28,17 @@ public class Number3 : MonoBehaviour
         if(sw)
         {
             num = Random.Range(0, 999);
-
+            numLabel.text = num.ToString("D3");
         }
         else
         {
-            if(num % 3 == 0)
+            if(num == -999)
+            {
+                resultLabel.text = "判定";
+                return;
+            }
+
+            if (num % 3 == 0)
             {
                 resultLabel.text = "勝ち";
             }
@@ -40,6 +46,12 @@ public class Number3 : MonoBehaviour
             {
                 resultLabel.text = "負け";
             }
+        }
+
+        // Enterキーでリセット
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            SceneManager.LoadScene(0);
         }
 
         

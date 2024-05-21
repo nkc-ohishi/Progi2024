45eef0868ece7ff7b5f60ab43bbba833eaba6277
ohishi_MSI2023 <gpu.kenji.ohishi@gmail.com>
2024-05-21 17:22:45 +0900

    コメント調整

--- Assets/Number3/Number3.cs
+++ Assets/Number3/Number3.cs
@@ -1,3 +1,8 @@
+//------------------------------------------------------------------------------
+// 科目：プログラミング技法
+// 内容：３の倍数なら勝ちシステム
+// 担当：Ken.D.Ohishi 2024.05.20
+//------------------------------------------------------------------------------
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,54 +11,65 @@ using UnityEngine.UI;
 
 public class Number3 : MonoBehaviour
 {
-    public Text numLabel;
-    public Text resultLabel;
+    public Text numLabel;       // 乱数表示用
+    public Text resultLabel;    // 勝ち or 負け表示用
+    public Text syouhaiLabel;   // 勝敗表示用
 
-    int num;
-    bool sw;
+    int num;                    // 乱数の値保存
+    int winNum;                 // 勝ち数
+    int loseNum;                // 負け数
+    int clickCnt;               // クリックされた回数カウント
 
     void Start()
     {
-        sw = false;
-        num = -999;
+        clickCnt = 0;
+        num      = -999;
+        winNum   = 0;
+        loseNum  = 0;
+        resultLabel.text = "判定";
     }
 
     void Update()
     {
+        // クリックされる毎にswの値をインクリメント
         if(Input.GetMouseButtonDown(0))
         {
-            sw = (sw) ? false : true;
+            clickCnt++;
         }
 
-        if(sw)
+        if(clickCnt == 1)
         {
+            // 乱数表示
             num = Random.Range(0, 1000);
             numLabel.text = num.ToString("D3");
         }
-        else
+        else if(clickCnt == 2)
         {
-            if(num == -999)
-            {
-                resultLabel.text = "判定";
-                return;
-            }
-
+            // 判定結果を更新
             if (num % 3 == 0)
             {
                 resultLabel.text = "勝ち";
+                winNum++;
             }
             else
             {
                 resultLabel.text = "負け";
+                loseNum++;
             }
+
+            // 判定結果を更新後、swを0に戻す
+            clickCnt = 0; 
         }
+
+        // 勝敗表示
+        string winstr  = winNum.ToString("D2");
+        string losestr = loseNum.ToString("D2");
+        syouhaiLabel.text = winstr + "勝" + losestr + "負";
 
         // Enterキーでリセット
-        if(Input.GetKeyDown(KeyCode.Return))
+        if (Input.GetKeyDown(KeyCode.Return))
         {
             SceneManager.LoadScene(0);
-        }
-
-        
+        }        
     }
 }
